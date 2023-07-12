ebe2995a9a4d454a2264c47100a6b5a2a614a2a8
Daniel Belle <henrique.danielb@gmail.com>
2023-07-11 23:09:18 -0300

    projeto blog

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/about', 'about')->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
